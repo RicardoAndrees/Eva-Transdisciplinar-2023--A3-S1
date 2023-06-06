90438de292f05feed3b2231ac6e2c8147ece7f31
Benjacantero <bcantero2023@alu.uct.cl>
2023-06-06 11:30:02 -0400

    Revert "este es una prueba del uso del git"
    This reverts commit b78e8cc331acee6188c221932efd872d05b3686f.

--- juegooo.py
+++ juegooo.py
@@ -6,8 +6,7 @@ import matplotlib.pyplot as plt
 
 # Función para verificar si se hizo clic en un botón
 def is_button_clicked(button_rect, mouse_pos):
-    #return button_rect.collidepoint(mouse_pos)
-    return
+    return button_rect.collidepoint(mouse_pos)
 
 # Generar el gráfico de la posición de la pelota
 def llamar_grafico():
